1ffbb2549a43d7dcd66967cf436fa041167292d0
laukintung322@gmail.com <laukintung322@gmail.com>
2022-09-08 09:58:02 +0800

    implementing context

--- Screen/Login.js
+++ Screen/Login.js
@@ -10,6 +10,16 @@ import {
   Button,
 } from "native-base";
 import { Formik } from "formik";
+import * as Yup from "yup";
+
+const onSubmit = (data) => {
+  console.log(data);
+};
+
+const validationSchema = Yup.object().shape({
+  Email: Yup.string().email().required("Email is Required"),
+  Password: Yup.string().required("Password is Required"),
+});
 
 const Login = () => {
   return (
@@ -18,27 +28,52 @@ const Login = () => {
         <Center>
           <Heading>Malaria Tracker</Heading>
         </Center>
-        <VStack mt={5} space={3}>
-          <FormControl>
-            <FormControl.Label>Email</FormControl.Label>
-            <Input/>
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Password</FormControl.Label>
-            <Input type="password" />
-            <Link
-              _text={{
-                fontSize: "md",
-                fontWeight: "500",
-                color: "indigo.500",
-              }}
-              alignSelf="flex-end"
-            >
-              Forget Password
-            </Link>
-          </FormControl>
-          <Button colorScheme="indigo">Login</Button>
-        </VStack>
+        <Formik
+          initialValues={{
+            Email: "",
+            Password: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+            <VStack mt={5} space={3}>
+              <FormControl isInvalid={"Email" in errors} isRequired>
+                <FormControl.Label>Email</FormControl.Label>
+                <Input
+                  onChangeText={handleChange("Email")}
+                  placeholder="Email"
+                />
+                <FormControl.ErrorMessage>
+                  {errors.Email}
+                </FormControl.ErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={"Password" in errors} isRequired>
+                <FormControl.Label>Password</FormControl.Label>
+                <Input
+                  type="password"
+                  onChangeText={handleChange("Password")}
+                />
+                <FormControl.ErrorMessage>
+                  {errors.Password}
+                </FormControl.ErrorMessage>
+                <Link
+                  _text={{
+                    fontSize: "md",
+                    fontWeight: "500",
+                    color: "indigo.500",
+                  }}
+                  alignSelf="flex-end"
+                >
+                  Forget Password
+                </Link>
+              </FormControl>
+              <Button colorScheme="indigo" onPress={handleSubmit}>
+                Login
+              </Button>
+            </VStack>
+          )}
+        </Formik>
       </Box>
     </Center>
   );
