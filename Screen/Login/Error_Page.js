5347c78b639288c3de938c32279fbc484e5250dd
lunatoys123 <laukintung322@gmail.com>
2023-02-03 09:56:55 +0800

    Implement error page

--- Screen/Login/Error_Page.js
+++ Screen/Login/Error_Page.js
@@ -1,11 +1,31 @@
-import { View, Text } from "native-base";
+import { Box, Text, VStack, Heading, Link } from "native-base";
 import React from "react";
+import { AntDesign } from "@expo/vector-icons";
 
-const Error_Page = () => {
+const Error_Page = props => {
 	return (
-		<View>
-			<Text>Error_Page</Text>
-		</View>
+		<Box safeArea alignSelf="center" py="8" width="90%">
+			<VStack alignItems="center" justifyContent="center" space={2}>
+				<AntDesign name="warning" size={100} color="red" />
+				<Heading>Error</Heading>
+				<Box bg="red.400" alignSelf="center" shadow="3">
+					<Text fontSize={16}>
+						This Account has been Blocked due to Security Reason, Please contact Your admin to get
+						further information
+					</Text>
+				</Box>
+				<Link
+					_text={{
+						fontSize: "md",
+						fontWeight: "500",
+						color: "indigo.500",
+					}}
+					onPress={()=> props.navigation.navigate("Login")}
+				>
+					Back to Login
+				</Link>
+			</VStack>
+		</Box>
 	);
 };
 
